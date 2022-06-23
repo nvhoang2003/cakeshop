1df7b12aa90a48af5fb4d235a18d3f25228d41e0
Luxury <quang.cd.1955@aptechlearning.edu.vn>
2022-06-23 00:17:16 -0700

    Add file

---
+++ resources/views/Admin/AdminWithRepos/AdminDetails.blade.php
@@ -0,0 +1,18 @@
+<dl class="row">
+    <dt class="col-sm-3">ID</dt>
+    <dd class="col-sm-9">{{ $admin->id }}</dd>
+
+    <dt class="col-sm-3">Username</dt>
+    <dd class="col-sm-9">{{ $admin->username }}</dd>
+
+    <dt class="col-sm-3">Contact</dt>
+    <dd class="col-sm-9">{{$admin->contact }}</dd>
+
+    <dt class="col-sm-3">Email</dt>
+    <dd class="col-sm-9">{{$admin->email }}</dd>
+
+    <dt class="col-sm-3">Password</dt>
+    <dd class="col-sm-9">{{$admin->password }}</dd>
+
+
+</dl>
